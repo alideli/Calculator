945ffb4c385a1008c39bd5d4ed657452d4f73392
Ali Delgarm <delgarm10@gmail.com>
2025-07-13 02:52:46 +0330

    add % and better README

--- Calculator.py
+++ Calculator.py
@@ -1,56 +1,91 @@
 def sum_ab(a,b):
-    if('.' not in a and '.' not in b):
-        print("{0} + {1} = {2}".format(int(float(a)), int(float(b)), (int(float(a))+int(float(b)))))
-    elif('.' in a and '.' in b):
-        print("{0} + {1} = {2}".format(float(a), float(b), (float(a)+float(b))))
-    elif('.' in a or '.' in b):
-        print("{0} + {1} = {2}".format(float(a), float(b), (float(a)+float(b))))
-    else:
-        print("Please enter number!")
+    try:
+        if('.' not in a and '.' not in b):
+            print("{0} + {1} = {2}".format(int(float(a)), int(float(b)), round((int(float(a))+int(float(b))), c)))
+        elif('.' in a and '.' in b):
+            print("{0} + {1} = {2}".format(float(a), float(b), round((float(a)+float(b)), c)))
+        elif('.' in a or '.' in b):
+            print("{0} + {1} = {2}".format(float(a), float(b), round((float(a)+float(b)), c)))
+        else:
+            print("Please enter number!")
+    except:
+        print("Error ===> Please specify decimal value!")
+        
 def sub_ab(a,b):
-    if('.' not in a and '.' not in b):
-        print("{0} - {1} = {2}".format(int(float(a)), int(float(b)), (int(float(a))-int(float(b)))))
-    elif('.' in a and '.' in b):
-        print("{0} - {1} = {2}".format(float(a), float(b), (float(a)-float(b))))
-    elif('.' in a or '.' in b):
-        print("{0} - {1} = {2}".format(float(a), float(b), (float(a)-float(b))))
-    else:
-        print("Please enter number!")
+    try:
+        if('.' not in a and '.' not in b):
+            print("{0} - {1} = {2}".format(int(float(a)), int(float(b)), round((int(float(a))-int(float(b))), c)))
+        elif('.' in a and '.' in b):
+            print("{0} - {1} = {2}".format(float(a), float(b), round((float(a)-float(b)), c)))
+        elif('.' in a or '.' in b):
+            print("{0} - {1} = {2}".format(float(a), float(b), round((float(a)-float(b)), c)))
+        else:
+            print("Please enter number!")
+    except:
+        print("Error ===> Please specify decimal value!")
     
 def mul_ab(a,b):
-    if('.' not in a and '.' not in b):
-        print("{0} * {1} = {2}".format(int(float(a)), int(float(b)), (int(float(a))*int(float(b)))))
-    elif('.' in a and '.' in b):
-        print("{0} * {1} = {2}".format(float(a), float(b), (float(a)*float(b))))
-    elif('.' in a or '.' in b):
-        print("{0} * {1} = {2}".format(float(a), float(b), (float(a)*float(b))))
-    else:
-        print("Please enter number!")
+    try:
+        if('.' not in a and '.' not in b):
+            print("{0} * {1} = {2}".format(int(float(a)), int(float(b)), round((int(float(a))*int(float(b))), c)))
+        elif('.' in a and '.' in b):
+            print("{0} * {1} = {2}".format(float(a), float(b), round((float(a)*float(b)), c)))
+        elif('.' in a or '.' in b):
+            print("{0} * {1} = {2}".format(float(a), float(b), round((float(a)*float(b)), c)))
+        else:
+            print("Please enter number!")
+    except:
+        print("Error ===> Please specify decimal value!")
 
 def div_ab(a,b):
-    if('.' not in a and '.' not in b):
-        print("{0} / {1} = {2}".format(int(float(a)), int(float(b)), (int(float(a))/int(float(b)))))
-    elif('.' in a and '.' in b):
-        print("{0} / {1} = {2}".format(float(a), float(b), (float(a)/float(b))))
-    elif('.' in a or '.' in b):
-        print("{0} / {1} = {2}".format(float(a), float(b), (float(a)/float(b))))
-    else:
-        print("Please enter number!")
+    try:
+        if('.' not in a and '.' not in b):
+            print("{0} / {1} = {2}".format(int(float(a)), int(float(b)), round((int(float(a))/int(float(b))), c)))
+        elif('.' in a and '.' in b):
+            print("{0} / {1} = {2}".format(float(a), float(b), round((float(a)/float(b)), c)))
+        elif('.' in a or '.' in b):
+            print("{0} / {1} = {2}".format(float(a), float(b), round((float(a)/float(b)), c)))
+        else:
+            print("Please enter number!")
+    except:
+        print("Error ===> Please specify decimal value!")
 
 def pow_ab(a,b):
-    if('.' not in a and '.' not in b):
-        print("{0} ** {1} = {2}".format(int(float(a)), int(float(b)), (int(float(a))**int(float(b)))))
-    elif('.' in a and '.' in b):
-        print("{0} ** {1} = {2}".format(float(a), float(b), (float(a)**float(b))))
-    elif('.' in a or '.' in b):
-        print("{0} ** {1} = {2}".format(float(a), float(b), (float(a)**float(b))))
-    else:
-        print("Please enter number!")
+    try:
+        if('.' not in a and '.' not in b):
+            print("{0} ** {1} = {2}".format(int(float(a)), int(float(b)), round((int(float(a))**int(float(b))), c)))
+        elif('.' in a and '.' in b):
+            print("{0} ** {1} = {2}".format(float(a), float(b), round((float(a)**float(b)), c)))
+        elif('.' in a or '.' in b):
+            print("{0} ** {1} = {2}".format(float(a), float(b), round((float(a)**float(b)), c)))
+        else:
+            print("Please enter number!")
+    except:
+        print("Error ===> Please specify decimal value!")
+
+def percentage(a,b):
+    try:
+        if('.' not in a and '.' not in b):
+            print("{0} % {1} = {2}".format(int(float(a)), int(float(b)), round((int(float(a))/100) * (int(float(b))), c)))
+        elif('.' in a and '.' in b):
+            print("{0} % {1} = {2}".format(float(a), float(b), round((float(a)/100) * (float(b)), c)))
+        elif('.' in a or '.' in b):
+            print("{0} % {1} = {2}".format(float(a), float(b), round(((float(a))/100) * (float(b)), c)))
+        else:
+            print("Please enter number!")
+    except:
+        print("Error ===> Please specify decimal value!")
+
 
 a = str(input("Please enter first number: "))
-b = str(input("Please enter second number: "))
-print("You can use \"+ - * / **\"")
+print("You can use \"+ - * / ** %\"")
 operate = str(input("Please enter your operate: "))
+b = str(input("Please enter second number: "))
+c_user = (input("Please enter decimal value (default: 4): "))
+try:
+    c = int(c_user)
+except:
+    c = 4
 
 def calculate():
     if(operate == '+'):
@@ -63,6 +98,8 @@ def calculate():
         div_ab(a,b)
     elif(operate == '**'):
         pow_ab(a,b)
+    elif(operate == '%'):
+        percentage(a,b)
     else:
         print("The operate you choose is wrong!")
 
--- README.md
+++ README.md
@@ -1,9 +1,44 @@
 # Calculator
-This project is a simple calculator.  
-How to use:  
-    numbers can be integer or decimal.  
-    enter your first number (for example "3")  
-    enter your second number (for example "6.4")  
-    enter your operate : " + - * / ** " for example ("+")  
-    " ** " is for power  
-    result ===> 3 ** 6.4 = 1131.2954233842977
+
+A simple Python calculator that supports basic arithmetic operations and allows the user to set the number of decimal places for the result.
+
+## Features
+- Addition (+)
+- Subtraction (-)
+- Multiplication (*)
+- Division (/)
+- Power (**)
+- Percentage (a % b calculates 'a percent of b')
+- User-defined decimal places for result display
+
+## How to Use
+1. Run the program.
+2. Enter the first number (integer or decimal).
+3. Enter the operator: one of "+ - * / ** %".
+4. Enter the second number.
+5. Enter the number of decimal places you want for the result (e.g., 2 or 4). If you enter nothing or an invalid value, the default is 4.
+6. The result will be displayed.
+
+### Example
+```
+Please enter first number: 12.5
+You can use "+ - * / ** %"
+Please enter your operate: *
+Please enter second number: 3
+Please enter decimal value (default: 4): 2
+12.5 * 3.0 = 37.50
+```
+
+## Notes
+- If you enter an invalid decimal value, the default (4) will be used.
+- For percentage, the first number is calculated as a percent of the second number.
+
+## Run
+To run the program, use the following command in your terminal:
+
+```
+python Calculator.py
+```
+
+## Author
+- alideli
